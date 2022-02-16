86adeabfc1c4a200ed42a19062b2eb4b49920fb0
Kostas <vivelakostas1096@gmail.com>
2022-02-16 12:39:32 +0300

    add first template and empty layouts

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -9,27 +9,13 @@ class BbsController extends Controller
 {
     public function index()
     {
-        $bbs = Bb::latest()->get();
-
-        $s = "Объявления\r\n\r\n";
-
-        foreach ($bbs as $bb) {
-            $s .= $bb->title . "\r\n";
-            $s .= $bb->price . " руб.\r\n";
-            $s .= "\r\n";
-        };
-
-        return response($s)
-            ->header('Content-Type', 'text/plain');
+        $context = ['bbs' => Bb::latest()->get()];
+        // dd($context);
+        return view('index', $context);
     }
 
     public function detail(Bb $bb)
     {
-        $s = $bb->title . "\r\n\r\n";
-        $s .= $bb->content . "\r\n";
-        $s .= $bb->price . " руб.\r\n";
-
-        return response($s)
-            ->header('Content-Type', 'text/plain');
+       return view('detail', ['bb' => $bb]);
     }
 }
---
+++ resources/views/detail.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Обявления</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="my-3 text_center">Объявления</h1>
+        <h2>{{ $bb->title }}</h2>
+        <p>{{ $bb->content }}</p>
+        <p>{{ $bb->price }}</p>
+        <p><a href="/">На перечень Обявлений</a></p>
+    </div>
+</body>
+
+</html>
