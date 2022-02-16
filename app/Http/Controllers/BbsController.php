ab9d50cd0276cbf973b762b03f9e4374f370c95e
Kostas <vivelakostas1096@gmail.com>
2022-02-16 11:58:45 +0300

    Add detail (show) of Bb

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -13,11 +13,21 @@ class BbsController extends Controller
 
         $s = "Объявления\r\n\r\n";
 
-        foreach($bbs as $bb) {
+        foreach ($bbs as $bb) {
             $s .= $bb->title . "\r\n";
             $s .= $bb->price . " руб.\r\n";
             $s .= "\r\n";
         };
+
+        return response($s)
+            ->header('Content-Type', 'text/plain');
+    }
+
+    public function detail(Bb $bb)
+    {
+        $s = $bb->title . "\r\n\r\n";
+        $s .= $bb->content . "\r\n";
+        $s .= $bb->price . " руб.\r\n";
 
         return response($s)
             ->header('Content-Type', 'text/plain');
