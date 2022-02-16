7dbab2a3224fddb26ac2e1c73e76ad1d5bae86dc
Kostas <vivelakostas1096@gmail.com>
2022-02-16 10:26:05 +0300

    Add Bb Model, Bbs Controller and bbs table

---
+++ app/Http/Controllers/BbsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bb;
+use Illuminate\Http\Request;
+
+class BbsController extends Controller
+{
+    public function index()
+    {
+        $bbs = Bb::latest()->get();
+
+        $s = "Объявления\r\n\r\n";
+
+        foreach($bbs as $bb) {
+            $s .= $bb->title . "\r\n";
+            $s .= $bb->price . " руб.\r\n";
+            $s .= "\r\n";
+        };
+
+        return response($s)
+            ->header('Content-Type', 'text/plain');
+    }
+}
