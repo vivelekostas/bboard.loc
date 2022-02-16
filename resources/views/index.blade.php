a7b1c0df5a6b7153b3b25c52167d388b4dfe42ce
Kostas <vivelakostas1096@gmail.com>
2022-02-16 12:59:06 +0300

    moved the common code to the layout and named the routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,42 +1,30 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.base')
 
-<head>
-    <meta charset="UTF-8">
-    <title>Обявления</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-</head>
+@section('main')
 
-<body>
-    <div class="container">
-        <h1 class="my-3 text-center">Объявления</h1>
-        @if(count($bbs) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Товар</th>
-                    <th>Цена, руб</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($bbs as $bb)
-                <tr>
-                    <td>
-                        <h3>{{ $bb->title }}</h3>
-                    </td>
-                    <td>{{ $bb->price }}</td>
-                    <td>
-                        <a href="/{{ $bb->id }}">Подробнее</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        @endif
-    </div>
-</body>
+@if(count($bbs) > 0)
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Товар</th>
+            <th>Цена, руб</th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($bbs as $bb)
+        <tr>
+            <td>
+                <h3>{{ $bb->title }}</h3>
+            </td>
+            <td>{{ $bb->price }}</td>
+            <td>
+                <a href="{{ route('detail', ['bb' => $bb->id]) }}">Подробнее</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endif
 
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [BbsController::class, 'index']);
-Route::get('/{bb}', [BbsController::class, 'detail']);
+Route::get('/', [BbsController::class, 'index'])->name('index');
+Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
